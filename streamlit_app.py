344001e99664ba96914f15699b280d064eda438a
Haruka-Tanaka0811 <pink_choo_love@icloud.com>
2025-06-15 16:10:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 st.title("宇宙天気ミニダッシュボード")
 st.write("NASAのAPIから、リアルタイムで宇宙天気情報を取得しています✨")
 
-API_KEY = "Z4lRtyMc91Hjew51Emf82OPCpNDkpdxJoHpJBLc5"  # ← ここに自分のキーを貼り付ける！
+API_KEY = st.secrets["NASA_API_KEY"]
 URL = f"https://api.nasa.gov/DONKI/FLR?startDate=2024-06-01&api_key={API_KEY}"
 
 response = requests.get(URL)
